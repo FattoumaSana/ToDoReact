0cdf70d1f8922d23d51114fecb47e7f2654fe2e1
sana <benissia.fs@gmail.com>
2025-02-24 12:06:12 +0100

    v1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './Redux/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client"; // Import from 'react-dom/client'
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
